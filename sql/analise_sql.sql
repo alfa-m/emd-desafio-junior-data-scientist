d46a2201b4047ee8b935ce02bdfb23f0f040d055
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-08-30 09:05:05 -0300

    adiciona perguntas SQL

---
+++ sql/analise_sql.sql
@@ -0,0 +1,30 @@
+-- 1 - Quantos chamados foram abertos no dia 01/04/2023?
+
+
+-- 2 - Qual o tipo de chamado que teve mais teve chamados abertos no dia 01/04/2023?
+
+
+-- 3 - Quais os nomes dos 3 bairros que mais tiveram chamados abertos nesse dia?
+
+
+-- 4 - Qual o nome da subprefeitura com mais chamados abertos nesse dia?
+
+
+-- 5 - Existe algum chamado aberto nesse dia que não foi associado a um bairro ou subprefeitura na tabela de bairros? Se sim, por que isso acontece?
+
+
+-- 6 - Quantos chamados com o subtipo "Perturbação do sossego" foram abertos desde 01/01/2022 até 31/12/2023 (incluindo extremidades)?
+
+
+-- 7 - Selecione os chamados com esse subtipo que foram abertos durante os eventos contidos na tabela de eventos (Reveillon, Carnaval e Rock in Rio).
+
+
+-- 8 - Quantos chamados desse subtipo foram abertos em cada evento?
+
+
+-- 9 - Qual evento teve a maior média diária de chamados abertos desse subtipo?
+
+
+-- 10 - Compare as médias diárias de chamados abertos desse subtipo durante os eventos específicos (Reveillon, Carnaval e Rock in Rio) e a média diária de chamados abertos desse subtipo considerando todo o período de 01/01/2022 até 31/12/2023.
+
+
